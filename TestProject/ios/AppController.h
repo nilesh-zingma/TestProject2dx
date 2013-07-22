cda473b1ffcacbaf8818d019b7bda86e705369aa
Nilesh Patel <nileshpatel@ADC-Clarions-Mac-mini.local>
2013-07-22 12:19:01 +0530

    Initial Commit

---
+++ TestProject/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TestProjectAppController.h
+//  TestProject
+//
+//  Created by Nilesh Patel on 22/07/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TestProject/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestProjectAppController.h
+//  TestProject
+//
+//  Created by Nilesh Patel on 22/07/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
